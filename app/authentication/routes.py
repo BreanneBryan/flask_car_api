5dec1a8186631273ed25764a21f48a4abe275286
Breanne Bryan <breannebryan@Breannes-MacBook-Pro.local>
2023-08-19 14:03:27 -0500

    updating for render

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -4,8 +4,8 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 # imports for flask login 
 from flask_login import login_user, logout_user, LoginManager, current_user, login_required
-auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
+auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
 @auth.route('/signup', methods = ['GET', 'POST'])
 def signup():
@@ -13,8 +13,6 @@ def signup():
 
     try:
         if request.method == 'POST' and form.validate_on_submit():
-            first = form.first.data
-            last = form.last.data
             email = form.email.data
             password = form.password.data
             print(email, password)
@@ -22,15 +20,15 @@ def signup():
             user = User(email, password = password)
 
             db.session.add(user)
-            print('test')
             db.session.commit()
+
+
 
             flash(f'You have successfully created a user account {email}', 'User-created')
             return redirect(url_for('site.home'))
-        
     except:
         raise Exception('Invalid form data: Please check your form')
-    return render_template('signup.html', form=form)
+    return render_template('sign_up.html', form=form)
 
 @auth.route('/signin', methods = ['GET', 'POST'])
 def signin():
@@ -45,14 +43,14 @@ def signin():
             logged_user = User.query.filter(User.email == email).first()
             if logged_user and check_password_hash(logged_user.password, password):
                 login_user(logged_user)
-                flash('Welcome!', 'auth-success')
+                flash('You were successful in your initiation. Congratulations, and welcome to the Jedi Knights', 'auth-sucess')
                 return redirect(url_for('site.profile'))
             else:
-                flash('You do not have access to this content.', 'auth-failed')
+                flash('You have failed in your attempt to access this content', 'auth-failed')
                 return redirect(url_for('auth.signin'))
     except:
-        raise Exception('Invalid Form Data: Please check your form')
-    return render_template('signin.html', form=form)
+        raise Exception('Invalid Form Data: Please Check your Form')
+    return render_template('sign_in.html', form=form)
 
 @auth.route('/logout')
 def logout():
